f67c9ab0bc0cdea582b748b99a4d6faed6b24db5
ikusteu <ivankust57@gmail.com>
2024-12-12 11:35:17 +0100

    create a db schema

---
+++ src/schemas/init.sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXISTS annotation (
+	id INTEGER NOT NULL,
+  x DECIMAL,
+  y DECIMAL,
+  z DECIMAL,
+  content TEXT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS operator (
+	id INTEGER NOT NULL,
+	name TEXT,
+	surname TEXT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS annotation_operator (
+  annotation_id INTEGER NOT NULL,
+  operator_id INTEGER NOT NULL,
+	PRIMARY KEY (annotation_id, operator_id)
+);
+
+CREATE TABLE IF NOT EXISTS model_annotation (
+  model_id INTEGER NOT NULL,
+  annotation_id INTEGER NOT NULL,
+	PRIMARY KEY (model_id, annotation_id)
+);
